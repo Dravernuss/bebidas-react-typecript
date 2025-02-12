4128a29208802accfec39c0f54104cf13c6546e2
Dravernuss <esteban16.rodas@gmail.com>
2025-02-11 21:59:30 -0500

    Notification added

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,10 +2,17 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice";
+import {
+  NotificationSliceType,
+  createNotificationSlice,
+} from "./notificationSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
+export const useAppStore = create<
+  RecipesSliceType & FavoritesSliceType & NotificationSliceType
+>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
   }))
 );
